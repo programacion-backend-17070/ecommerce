c69401d6c283187dadc5652b979df52b180dc0b8
Eduardo Velazquez <lalo0892@gmail.com>
2022-05-25 13:07:26 +0200

    data on wire

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios"
+
 function App() {
-  
+  const [products, setProducts] = useState([])
+
+  const getProducts = () => {
+    console.log('getProducts')
+    // return [{ name: 1 }]
+    axios.get("http://localhost:8080/api/products")
+      .then(({ data }) => setProducts(data))
+    
+  }
+
+  useEffect(getProducts, [])
+
   return (
     <div className="App">
-      <p>Hola mundo</p>
+      {products.map((c, id)=> <p key={id}>{c.name}</p>)}
     </div>
   );
 }
