8ce2b78c571ee31595953364cde8ec2c46488cfc
Eduardo Velazquez <lalo0892@gmail.com>
2022-06-04 14:48:58 +0200

    api products controller

--- src/controllers/api.products.controller.js
+++ src/controllers/api.products.controller.js
@@ -1,1 +1,36 @@
-module.exports = {}
+const productModel = require('../models/product.model')
+
+module.exports = {
+  getAll: async (req, res) => res.send(await productModel.getAll()),
+  post: async (req, res) => {
+    const { body } = req
+
+    const newProductId = await productModel.save(body)
+
+    return res.status(201).send(newProductId)
+  },
+  get: async (req, res) => {
+    if (!req.params.id) {
+      return res.sendStatus(404)
+    }
+    const product = await productModel.getById(req.params.id)
+
+    if (!product) {
+      return res.sendStatus(404)
+    }
+
+    return res.send(product)
+  },
+  delete: async (req, res) => {
+    const { id } = req.params
+
+    if (!id) {
+      return res.sendStatus(404)
+    }
+
+    await productModel.delete(id)
+
+    return res.sendStatus(200)
+  },
+  put: (req, res) => {},
+}
